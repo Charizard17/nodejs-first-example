d4ee695323e9ef317ff6ac5d29b8f0d518fc38fc
Charizard17 <3723esad@gmail.com>
2020-05-30 21:55:02 +0200

    event & http module added & public files created

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const Person = require("./person");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const person1 = new Person("John Doe", 30);
-const person2 = new Person("Killy Filly", 27);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+          if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+});
 
-person1.greeting();
-person2.greeting();
+const PORT = 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- person.js
+++ person.js
@@ -1,12 +1,19 @@
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
+// Module Wrapper Function
+// (function (exports, require, module, __filename,__dirname) {
 
-    greeting() {
-        console.log(`My name is ${this.name} and I am ${this.age}`)
-    }
+// })
+
+// console.log(__dirname, __filename);
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greeting() {
+    console.log(`My name is ${this.name} and I am ${this.age}`);
+  }
 }
 
 module.exports = Person;
